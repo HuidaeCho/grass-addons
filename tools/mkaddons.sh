6e865439f94f1d3d53cfaec4a308e00ce2557fe9
Huidae Cho <grass4u@gmail.com>
2022-03-24 12:27:03 -0400

    Add mkaddons.sh

---
+++ tools/mkaddons.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# This script builds GRASS GIS addons for the current architecture.
+
+set -e
+. ${GRASSBUILDRC-~/.grassbuildrc}
+cd $GRASS_ADDONS_SRC
+
+grass_build_scripts=$HOME/usr/local/src/grass-build-scripts
+arch=`$grass_build_scripts/switcharch.sh --query`
+
+make \
+MODULE_TOPDIR=$GRASS_SRC \
+LIBREDWGLIBPATH=-L$LIBREDWG_LIB \
+LIBREDWGINCPATH=-I$LIBREDWG_INC \
+"$@" > $GRASS_SRC/mkaddons.log 2>&1
+
+cd $GRASS_SRC
+for i in \
+	mkaddons.log \
+	error.log \
+; do
+	cp -a $i $i.$arch
+done
